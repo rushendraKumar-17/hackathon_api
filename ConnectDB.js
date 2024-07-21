fa94ed1fa95097d5d9c3342a186538dbd963b975
Rushendra Kumar <rushendrakumar17@gmail.com>
2024-07-21 15:49:14 +0530

    commit

--- ConnectDB.js
+++ ConnectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  mongoose.connect("mongodb://localhost:27017/Hackathon")
+  mongoose.connect("mongodb+srv://rushi17092004:rushi123@cluster0.4bc8xaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(()=>console.log("connected successfully"))
   .catch(err=>console.log(err));
 };
